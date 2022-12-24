b914873268c297322905b5abd80d2f5706be813e
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-24 22:13:01 +0530

    mainFile is added

---
+++ Main.java
@@ -0,0 +1,7 @@
+package classProblemByAmolMateSir.Day15PracticeProblem;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("welcome to hashTable");
+    }
+}
